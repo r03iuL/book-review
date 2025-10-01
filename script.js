6dfeb3d91ca7e9e1a57706082ff68059456fad8e
r03iuL <robiulislam1806@gmail.com>
2025-10-02 00:14:55 +0600

    feat: add mobnile nav menue

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 console.log("Script loaded");
 
 /*===== MENU =====*/
+const hamburger = document.getElementById('hamburger-btn');
+const navMenu = document.querySelector('.nav-container');
 
+hamburger.addEventListener('click',()=>{
+    navMenu.classList.toggle('active');
+});
 /*===== GSAP ANIMATION =====*/
 // NAV
 
